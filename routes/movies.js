9af2ca33e7a17d29ac554093993bd0578a2ceac1
aslimaye90 <aslimaye90@gmail.com>
2015-02-22 19:25:21 -0800

    search movies based on keywords, browse movies by pages successful

--- routes/movies.js
+++ routes/movies.js
@@ -2,20 +2,11 @@ var express = require('express');
 var router = express.Router();
 var pool = require('./MySQLConnectionPool')
 
-/* GET home page. */
+/* GET movies home page. */
 router.get('/', function(req, res) {
 
-	//var x = req.params.letter;
   var queryString = 'select MovieName, category from MovieList LIMIT 100';
   var countQuery = 'select count(*) AS count from MovieList';
-
-  console.log(req.params.pageNumber);
-
-  /*if(!(typeof req.body.pageNumber === 'undefined')){
-    var x = 100*req.body.pageNumber;
-    queryString = 'select MovieName, category from MovieList LIMIT '+x+', 100';
-    console.log("changed");
-  }*/
 
 	pool.getConnection(function(err,connection){
 		if (err){throw err;}
@@ -26,7 +17,7 @@ router.get('/', function(req, res) {
       connection.query(queryString, function(err, rows, fields) {
         if (err){throw err;}      
 
-        res.render('pages/moviesNew',
+        res.render('pages/movies',
         { 
             title: 'Movie Page', 
             data: JSON.parse(JSON.stringify(rows)),
@@ -39,12 +30,12 @@ router.get('/', function(req, res) {
 });
 
 
-
+/* GET movies based on keywords*/
 router.get('/:searchReq', function(req, res) {
 
   var x = req.params.searchReq;
-  var queryString = 'select MovieName, category from MovieList where MovieName LIKE "'+x+'%" LIMIT 100';
-  var countQuery = 'select count(*) AS count from MovieList where MovieName LIKE "'+x+'%"';
+  var queryString = 'select MovieName, category from MovieList where MovieName LIKE "%'+x+'%" LIMIT 100';
+  var countQuery = 'select count(*) AS count from MovieList where MovieName LIKE "%'+x+'%"';
 
   pool.getConnection(function(err,connection){
     if (err){throw err;}
@@ -53,17 +44,41 @@ router.get('/:searchReq', function(req, res) {
       if(err){throw err;}
 
       connection.query(queryString, function(err, rows, fields) {
-      if (err){throw err;}      
-          res.render('pages/movies',
-          { 
-              title: 'Movie Page', 
-              data : JSON.parse(JSON.stringify(rows)),
-              rows: totRows[0].count
-          });
-        });
+        if (err){throw err;}     
+        res.json({data: rows, totRows: totRows[0].count});
+      });
     });
     connection.release();
+  });
+});
+
+
+/*GET different pages for the same search request*/
+router.get('/:searchReq/:pgNo', function(req, res) {
+
+  var x = req.params.searchReq;
+  var pgNo = req.params.pgNo;
+  var queryString = 'select MovieName, category from MovieList where MovieName LIKE "%'+x+'%" LIMIT '+pgNo+', 100';
+  var countQuery = 'select count(*) AS count from MovieList where MovieName LIKE "%'+x+'%"';
+
+  if(req.params.searchReq == "secret987asdhfqpw8oapi"){
+    queryString = 'select MovieName, category from MovieList LIMIT '+pgNo+', 100';
+    countQuery = 'select count(*) AS count from MovieList';
+  }
+
+  pool.getConnection(function(err,connection){
+    if (err){throw err;}
+
+    connection.query(countQuery, function(err, totRows){
+      if(err){throw err;}
+
+      connection.query(queryString, function(err, rows, fields) {
+        if (err){throw err;}     
+        res.json({data: rows, totRows: totRows[0].count});
+      });
     });
+    connection.release();
+  });
 });
 
 module.exports = router;
